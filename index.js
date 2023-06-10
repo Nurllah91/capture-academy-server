2c97c0dbcd1c6c65578addfc08760726b6c7b53f
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-10 20:04:12 +0600

    jwt token added and verify jwt aded

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    res.status(401).send({ error: true, message: "unauthorized access" })
+  }
+
+  // barer token
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: "invalid token" })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
 
 
 // mongodb database connect
@@ -36,6 +54,14 @@ async function run() {
     const classCollection = client.db("capture-academy").collection("classes");
 
 
+    // JWT TOKEN created
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '1d' })
+      res.send({ token });
+    })
+
+
     // users api
     app.get('/users', async (req, res) => {
       const result = await userCollection.find().toArray();
@@ -43,14 +69,14 @@ async function run() {
     })
 
     // instructor api
-    app.get('/instructors/popular', async(req, res)=>{
+    app.get('/instructors/popular', async (req, res) => {
       const result = await userCollection.find().limit(6).toArray();
       res.send(result);
     })
 
-    
-    app.get('/instructors', async(req, res)=>{
-      const query = {role: "Instructor"};
+
+    app.get('/instructors', async (req, res) => {
+      const query = { role: "Instructor" };
       const result = await userCollection.find(query).toArray();
       res.send(result);
     })
