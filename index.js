0a42a06756c93262ff14c0bbbc86adea43954055
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-12 00:08:36 +0600

    delete classes added

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const verifyJWT = (req, res, next) => {
 
 
 // mongodb database connect
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gspcn8d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -178,6 +178,15 @@ async function run() {
       const query = {email: email};
       const result = await selectedClassCollection.find(query).toArray();
       res.send(result);
+    })
+
+    // delete selected class 
+      app.delete('/selected-class/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await selectedClassCollection.deleteOne(query);
+
+        res.send(result);
     })
 
 
